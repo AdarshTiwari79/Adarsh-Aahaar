f515d55a076294f6da925a8ffe3ccc168eab66e7
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-09-18 13:49:13 +0530

    complete restaurant ui

--- Client/src/admin/Restaurant.tsx
+++ Client/src/admin/Restaurant.tsx
@@ -1,84 +1,172 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {
+  RestaurantFormSchema,
+  restaurantFromSchema,
+} from "@/schema/restaurantSchema";
 import { Loader2 } from "lucide-react";
+import { FormEvent, useState } from "react";
 
 const Restaurant = () => {
+  const [input, setInput] = useState<RestaurantFormSchema>({
+    restaurantName: "",
+    city: "",
+    country: "",
+    deliveryTime: 0,
+    cuisines: [],
+    imageFile: undefined,
+  });
+
+  const changeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type } = e.target;
+    setInput({ ...input, [name]: type === "number" ? Number(value) : value });
+  };
+
+  const [errors, setErrors] = useState<Partial<RestaurantFormSchema>>({});
+
   const loading = false;
+  const restaurant = false;
+
+  const onSubmitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const result = restaurantFromSchema.safeParse(input);
+    if (!result.success) {
+      const fieldError = result.error.formErrors.fieldErrors;
+      setErrors(fieldError as Partial<RestaurantFormSchema>);
+      return;
+    }
+
+    console.log(input);
+    // api implementation
+  };
 
   return (
     <div className="max-w-6xl mx-auto my-10">
       <div>
         <div>
           <h1 className="font-extrabold text-2xl mb-5">Add Restaurant</h1>
-          <form>
+          <form onSubmit={onSubmitHandler}>
             <div className="md:grid grid-cols-2 gap-6 space-y-2 md:space-y-0">
               {/* Restaurant Name */}
               <div>
                 <Label>Restaurant Name</Label>
                 <Input
                   type="text"
-                  value={}
+                  value={input.restaurantName}
+                  onChange={changeEventHandler}
                   name="restaurantName"
                   placeholder="Enter your restaurant name"
                 />
+                {errors && (
+                  <span className="text-xs text-red-600 font-medium">
+                    {errors.restaurantName}
+                  </span>
+                )}
               </div>
 
               <div>
                 <Label>City</Label>
                 <Input
                   type="text"
-                  value={}
+                  value={input.city}
+                  onChange={changeEventHandler}
                   name="city"
                   placeholder="Enter your city name"
                 />
+                {errors && (
+                  <span className="text-xs text-red-600 font-medium">
+                    {errors.city}
+                  </span>
+                )}
               </div>
 
               <div>
                 <Label>Country</Label>
                 <Input
                   type="text"
-                  value={}
+                  value={input.country}
+                  onChange={changeEventHandler}
                   name="country"
                   placeholder="Enter your country name"
                 />
+                {errors && (
+                  <span className="text-xs text-red-600 font-medium">
+                    {errors.country}
+                  </span>
+                )}
               </div>
 
               <div>
                 <Label>Delivery Time</Label>
                 <Input
-                  type="text"
-                  value={}
+                  type="number"
+                  value={input.deliveryTime}
+                  onChange={changeEventHandler}
                   name="deliveryTime"
                   placeholder="Enter your delivery time"
                 />
+                {errors && (
+                  <span className="text-xs text-red-600 font-medium">
+                    {errors.deliveryTime}
+                  </span>
+                )}
               </div>
 
               <div>
                 <Label>Cuisines</Label>
                 <Input
                   type="text"
-                  value={}
+                  value={input.cuisines}
+                  onChange={(e) =>
+                    setInput({ ...input, cuisines: e.target.value.split(",") })
+                  }
                   name="cuisines"
-                  placeholder="Enter your cusines"
+                  placeholder="e.g. Momos, Biryani"
                 />
+                {errors && (
+                  <span className="text-xs text-red-600 font-medium">
+                    {errors.cuisines}
+                  </span>
+                )}
               </div>
 
               <div>
                 <Label>Upload Restaurant Banner</Label>
-                <Input type="file" value={} name="image" accept="image/*" />
+                <Input
+                  onChange={(e) =>
+                    setInput({
+                      ...input,
+                      imageFile: e.target.files?.[0] || undefined,
+                    })
+                  }
+                  type="file"
+                  name="imageFile"
+                  accept="image/*"
+                />
+                {errors && (
+                  <span className="text-xs text-red-600 font-medium">
+                    {errors.imageFile?.name}
+                  </span>
+                )}
               </div>
             </div>
 
-            <div>
+            <div className="my-5 w-fit">
               {loading ? (
                 <Button disabled className="bg-orange hover:bg-hoverOrange">
                   <Loader2 className="mr-2 h-4 w-4 animate-spin" />
                   Please Wait
                 </Button>
               ) : (
-                <Button className="bg-orange hover:bg-hoverOrange">
-                  Add Your Restaurant
+                <Button
+                  type="submit"
+                  className="bg-orange hover:bg-hoverOrange"
+                >
+                  {restaurant
+                    ? "Update Your Restaurant"
+                    : "Add Your Restaurant"}
                 </Button>
               )}
             </div>
