cd4ed71021b7ed2351dad77e92e58ecfbf751037
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-09-23 12:09:46 +0530

    default middleware and user api implementation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,29 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
-import connectDB from './db/connectDB';
+import connectDB from "./db/connectDB";
+import userRoute from "./routes/user.route";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// default middlware for any mern porject
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+// api
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, () => {
   connectDB();
