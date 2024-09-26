2cf0afb7665c810019a6d69431f074a5883f9228
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2024-09-26 13:08:21 +0530

    type declaration and api implementation for forgot password and all

--- Client/src/store/useUserStore.ts
+++ Client/src/store/useUserStore.ts
@@ -8,7 +8,34 @@ const API_END_POINT = "http://localhost:8000/api/v1/user";
 
 axios.defaults.withCredentials = true;
 
-export const useUserStore = create<any>()(
+type User = {
+  fullname: string;
+  email: string;
+  contact: string;
+  address: string;
+  city: string;
+  country: string;
+  profilePicture: string;
+  admin: boolean;
+  isVerified: boolean;
+};
+
+type UserState = {
+  user: User | null;
+  isAuthenticated: boolean;
+  isCheckingAuth: boolean;
+  loading: boolean;
+  signup: (input: SignupInputState) => Promise<void>;
+  login: (input: LoginInputState) => Promise<void>;
+  verifyEmail: (verificationCode: string) => Promise<void>;
+  checkAuthentication: () => Promise<void>;
+  logout: () => Promise<void>;
+  forgotPassword: (email: string) => Promise<void>;
+  resetPassword: (token: string, newPassword: string) => Promise<void>;
+  updateProfile: (input: any) => Promise<void>;
+};
+
+export const useUserStore = create<UserState>()(
   persist(
     (set) => ({
       user: null,
@@ -91,6 +118,104 @@ export const useUserStore = create<any>()(
         } catch (error: any) {
           set({ loading: false });
           toast.error(error.response.data.message);
+        }
+      },
+
+      checkAuthentication: async () => {
+        try {
+          set({ isCheckingAuth: true });
+          const response = await axios.get(`${API_END_POINT}/check-auth`);
+
+          if (response.data.success) {
+            set({
+              loading: false,
+              user: response.data.user,
+              isAuthenticated: true,
+              isCheckingAuth: false,
+            });
+          }
+        } catch (error) {
+          set({
+            loading: false,
+            isAuthenticated: false,
+            isCheckingAuth: false,
+          });
+        }
+      },
+
+      logout: async () => {
+        try {
+          set({ loading: true });
+          const response = await axios.post(`${API_END_POINT}/logout`);
+          if (response.data.success) {
+            toast.success(response.data.message);
+            set({ loading: false, isAuthenticated: false, user: null });
+          }
+        } catch (error: any) {
+          set({ loading: false });
+          toast.error(error.response.data.message);
+        }
+      },
+
+      forgotPassword: async (email: string) => {
+        try {
+          set({ loading: true });
+          const response = await axios.post(
+            `${API_END_POINT}/forgot-password`,
+            { email }
+          );
+          if (response.data.success) {
+            toast.success(response.data.message);
+            set({ loading: false });
+          }
+        } catch (error: any) {
+          toast.error(error.response.data.message);
+          set({ loading: false });
+        }
+      },
+
+      resetPassword: async (token: string, newPassword: string) => {
+        try {
+          set({ loading: true });
+          const response = await axios.post(
+            `${API_END_POINT}/reset-password/${token}`,
+            { newPassword }
+          );
+
+          if (response.data.success) {
+            toast.success(response.data.success);
+            set({ loading: false });
+          }
+        } catch (error: any) {
+          toast.error(error.response.data.message);
+          set({ loading: false });
+        }
+      },
+
+      updateProfile: async (input: any) => {
+        try {
+          set({ loading: true });
+          const response = await axios.put(
+            `${API_END_POINT}/profile/update`,
+            input,
+            {
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+
+          if (response.data.success) {
+            toast.success(response.data.message);
+            set({
+              loading: false,
+              user: response.data.user,
+              isAuthenticated: true,
+            });
+          }
+        } catch (error: any) {
+          toast.error(error.response.data.message);
+          set({ loading: false });
         }
       },
     }),
