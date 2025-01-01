9b531a88ab574a184a0cf8e361e0b5620d83f259
AdarshTiwari79 <jaunpuradarsh62@gmail.com>
2025-01-01 17:14:50 +0530

    Get, Update and Search Restaurant methods definition

--- Client/src/store/useRestaurantStore.ts
+++ Client/src/store/useRestaurantStore.ts
@@ -14,10 +14,13 @@ axios.defaults.withCredentials = true;
 
 // }
 
-export const useRestaurantStore = create()(
+export const useRestaurantStore = create<any>()(
   persist(
     (set) => ({
       loading: false,
+      restaurant: null,
+      searchedRestaurant: null,
+
       createRestaurant: async (formData: FormData) => {
         try {
           set({ loading: true });
@@ -36,6 +39,55 @@ export const useRestaurantStore = create()(
           set({ loading: false });
         }
       },
+
+      getRestaurant: async () => {
+        try{
+          set({loading:true});
+          const response = await axios.get(`${API_END_POINT}/`);
+          if(response.data.success){
+            set({loading:false,restaurant:response.data.restaurant});
+          }
+        } catch(error:any){
+          if(error.response.status === 404){
+            set({restaurant:null});
+          }
+          set({loading:false})
+        }
+      },
+
+      updateRestaurant: async (formData: FormData) => {
+        try{
+          set({loading:true});
+          const response = await axios.put(`${API_END_POINT}/`,formData,{
+            headers:{
+              "Content-Type":"multipart/form-data"
+            }
+          })
+          if(response.data.success){
+            toast.success(response.data.message);
+            set({loading:false});
+          }
+        } catch(error:any){
+          toast.error(error.response.data.message);
+          set({loading:false});
+        }
+      },
+
+      searchRestaurant: async (searchText:string, searchQuery:string, selectedCuisines:any) => {
+        try{
+          set({loading:true});
+          const params = new URLSearchParams();
+          params.set("searchQuery",searchQuery);
+          params.set("selectedCuisines",selectedCuisines);
+          const response = await axios.get(`${API_END_POINT}/search/${searchText}?${params.toString()}`);
+          if(response.data.success){
+            set({loading:false,searchedRestaurant:response.data});
+          }
+        } catch(error:any){
+          set({loading:false});
+        }
+      }
+
     }),
     {
       name: "restaurant-name",
